5588daafe283ef44352445d8a45dee591704fef3
Aditya <aditya@Adityas-MacBook-Pro.local>
2025-08-08 20:06:14 +0530

    add

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-const root = document.getElementById('root');
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
 
-const heading = document.createElement('h1');
-heading.textContent = 'Hello, World!';
-
-root.appendChild(heading);
+const rootElement = React.createElement("div", { className: "container" }, [
+  React.createElement("div", { key: "child-1", className: "child-1" }, "Child 1"),
+  React.createElement("div", { key: "child-2", className: "child-2" }, [
+    React.createElement("div", { key: 'child-2a', className: "child-2A" }, "Child 2A"),
+    React.createElement("div", { key: 'child-2b', className: "child-2B" }, "Child 2B"),
+  ]),
+]);
+console.log(rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(rootElement);
